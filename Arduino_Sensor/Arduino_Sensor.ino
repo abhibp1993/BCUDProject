93855a5230ae3f2cb8278e074445131df0796928
abhi.bp1993@gmail.com <abhi.bp1993@gmail.com>
2015-04-08 22:18:37 +0530

    Updated and Implemented all functions except IR-Protections. The code is ready for field testing on real arduinos, with all sensors connected.

--- Arduino_Sensor/Arduino_Sensor.ino
+++ Arduino_Sensor/Arduino_Sensor.ino
@@ -37,75 +37,13 @@ along with BCUDProject.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-//---------------------------------------------------------------------------------------------
-//Shruti's earlier code
 
-//#include <SharpIR.h>
-//#include <avr/wdt.h>
-
-//#define ir0 A0     
-//#define ir1 A1
-//#define ir2 A2
-//#define ir3 A3
-//Assign pin for Sharp input
-//#define model 20150          //Specify the model of sensor used
-
-
-//#define MINREAD 10          //Minimum distance read by Sharp
-//#define CLOSEOBSTACLE    0
-//boolean sharp_engage;       //Engages Sharp Protection
-
-//boolean closeobstable=false; //Gets true if obstacle is closer than MINREAD
-
-
-
-
-//SharpIR sharp(ir, 25, 93, model);
-
-// ir: the pin where your sensor is attached
-// 25: the number of readings the library will make before calculating a mean distance
-// 93: the difference between two consecutive measurements to be taken as valid
-// model: an int that determines your sensor:  1080 for GP2Y0A21Y
-//                                            20150 for GP2Y0A02Y
-//                                            (working distance range according to the datasheets)
-
-/*Program variables*/
-//float dis;
-
-/*User functions
-
-1. Updating Sharp readings
-Parameters: None
-  Returns: float
-  
-*/
-
-//float sharp_update(){
-//   dis=sharp.distance();  // this returns the distance to the obstacle
-//   return dis;
-//}
-
-/* Implements an algorithm which decides if system should be put in PROTECTION MODE based on
-   Sharp's-Proximity sensor readings.
-  [Read more about PROTECTION MODE in Programmer's Guide.]
-  
-  Parameters: None
-  Returns: "true" if PROTECTION MODE is to be activated, "false" otherwise. (data type: boolean)
-*/
-//boolean sharp_protect(){
-//  if(dis<MINREAD)
-//  {
-//    return true;
-//  }
-//}
-
-//void system_protect(uint8_t label){
-//  
-//}
-
-
-//---------------------------------------------------------------
+/**********************************************************************************/
 // Modified by Abhishek on 07 April 2015; 22:20 hours
+// Modified by Shruti on 08 April 2015; 00:15 hours
+// Modified by Abhishek on 08 April 2015; 20:12 hours
+/**********************************************************************************/
+
 
 #include <SharpIR.h>
 #include "Global.h"
@@ -199,33 +137,3 @@ void loop() {
    delay(1000);
   
 }
-
-
-  
-  
-  
-  
-//  wdt_reset();
-
-  // IR Proximity Sensing, Protection
-//  if(sharp_enable.0=1)
-//  { SharpValues[0]=sharp_update();}
-//  
-//  if(sharp_enable.1=1)
-//   { SharpValues[1]=sharp_update();}
-//   
-//  if(sharp_enable.2=1)
-//     {SharpValues[2]=sharp_update();}
-//     
-//  if(sharp_enable.3=1)
-//     {SharpValues[3]=sharp_update():}
-//   }
-//  }
-//  sharp_call();
-//  if (sharp_engage){
-//    if (sharp_protect()){
-//      system_protect(CLOSEOBSTACLE);
-//    }
-//  }
-
- 
