51e0537e5634683ae411430c39276de1ce9af0a3
abhi.bp1993@gmail.com <abhi.bp1993@gmail.com>
2015-04-11 23:51:00 +0530

    Modified Pin Assignments for Arduino_Sensor and Arduino_Control

--- Arduino_Control/global.h
+++ Arduino_Control/global.h
@@ -25,32 +25,70 @@ along with ProjectCurio.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+
+
+/*
+Conventions:
+	0. All definitions with PIN_*** are physical pin connections. Rest are software 
+		pre-processor definitions.
+	1. AM: Analog MUX  - 74HC4051
+	2. DM: Digital MUX - 74151
+	
+	
+Connections:
+	We utilize ADC functionality of 3 pins
+		A] Motor1_CurrSense; 					-->	A0
+		B] Motor2_CurrSense; 					--> A1
+		C] Analog Multiplexer's output (Y)		--> A2
+	
+	We utilize PWM functionality of 2 pins
+		A] Motor1_PWM							-->  9
+		B] Motor2_PWM							--> 10
+	
+	We keep Serial Communication pins available --> 0, 1
+	
+	We keep I2C Communications Pins available	--> A4, A5
+	
+	Rest all pins are used as digital input-output.
+	
+
+Remark:
+	The digital Pins as well as the analog pins(except A4, A5)
+	can be altered as per PCB design convenience. 
+	
+	Secondly, it would be nice if we could switch between I2C
+	and Serial Comm, using a 4-DIP switch configuration.
+
+*/
+
+
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
 
 
-#define PIN_AM_S0   A2   
-#define PIN_AM_S1   A3
-#define PIN_AM_S2   A4 
-#define PIN_AM_Y    A5
+#define PIN_AM_S0   		A5		// Analog Mux: Select 0   	
+#define PIN_AM_S1   		A3		// Analog Mux: Select 1
+#define PIN_AM_S2   		A4 		// Analog Mux: Select 2
+#define PIN_AM_Y    		A2		// Analog Mux: Output
 
-#define PIN_M1_CS          A0
-#define PIN_M1_IN1         11  //A6
-#define PIN_M1_IN2         12  //A7
-#define PIN_M1_PWM         9
-#define PIN_M1_ENCA        2
-#define PIN_M1_ENCB        4   // 8
+#define PIN_M1_CS           A0		// Current Sense
+#define PIN_M1_IN1          11  	// Direction Control pin 1
+#define PIN_M1_IN2          12  	// Direction Control pin 2
+#define PIN_M1_PWM          9		// Speed control pin (0-5V; 31Hz to 500Hz)
+#define PIN_M1_ENCA         2		// Encoder Channel A: High frequency signal
+#define PIN_M1_ENCB         4   	// Encoder Channel B: High frequency signal
 
-#define PIN_M2_CS          A1
-#define PIN_M2_IN1         6
-#define PIN_M2_IN2         5
-#define PIN_M2_PWM         10
-#define PIN_M2_ENCA        8  //3
-#define PIN_M2_ENCB        7  //6
+#define PIN_M2_CS           A1		// Current Sense
+#define PIN_M2_IN1          6		// Direction Control pin 1
+#define PIN_M2_IN2          5		// Direction Control pin 2
+#define PIN_M2_PWM          10		// Speed control pin (0-5V; 31Hz to 500Hz)
+#define PIN_M2_ENCA         8  		// Encoder Channel A: High frequency signal
+#define PIN_M2_ENCB         7  		// Encoder Channel B: High frequency signal
 
-#define PIN_DM_S0          A4  //11
-#define PIN_DM_S1          A5  //12
-#define PIN_DM_Y           3
+#define PIN_DM_S0           A6 		// Digital Mux: Select 0
+#define PIN_DM_S1           A7 		// Digital Mux: Select 1	
+#define PIN_DM_Y            3		// Digital Mux: Output	
+									// Digital Mux: Select 2 (is GND hard-wired)
 
 
 #endif 
--- Arduino_Sensor/Global.h
+++ Arduino_Sensor/Global.h
@@ -1,33 +1,101 @@
+/*
+File: Arduino_Sensor/Global.h
+
+This file defines the pin configuration for Arduino_Sensor.ino 
+
+Author: Abhishek N. Kulkarni
+Date Created: 06 April 2015
+Acknowledgements: Prof. Milind Patwardhan, Prof. Milind Kamble,
+  Prof. Mrunal Shidore, Monica Patel, Shruti Phadke, Aditya Joshi.
+
+
+This file is part of BCUDProject.
+
+BCUDProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BCUDProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with ProjectCurio.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+/*
+Conventions:
+	0. All definitions with PIN_*** are physical pin connections. Rest are software 
+		pre-processor definitions.
+	1. AM: Analog MUX  - 74HC4051
+	2. DM: Digital MUX - 74HC4051 (Will be used with digital signals only)
+	
+	
+Connections:
+	We utilize ADC functionality of 5 pins
+		A] Sharp 1								--> A0
+		B] Sharp 2 								--> A1
+		C] Sharp 3								--> A2
+		D] Sharp 4								--> A6	(optional)
+		E] Analog Multiplexer(AM -- Y)			--> A3
+		
+	We keep Serial Communication pins available --> 0, 1
+	
+	We keep I2C Communications Pins available	--> A4, A5
+	
+	Rest all pins are used as digital input-output.
+	
+
+Remark:
+	The digital Pins as well as the analog pins (except A4, A5)
+	can be altered and assigned as per PCB design convenience. 
+	
+	Secondly, it would be nice if we could switch between I2C
+	and Serial Comm, using a 4-DIP switch configuration.
+
+	This Arduino along with BBB provides the extension capability 
+	for IO. The pins which are not in use by Arduino all the time
+	might be assigned towards this objective.
+	
+*/
+
+
 #ifndef GLOBAL_H_
 #define GLOBAL_H_
 
 #define SHARP_MODEL      20150
 
-#define PIN_SHARP_1      A0
-#define PIN_SHARP_2      A1
-#define PIN_SHARP_3      A2
-#define PIN_SHARP_4      A3    // EXTENSION PIN IF SHARP NOT CONNECTED
+#define PIN_SHARP_1      A0		// Analog Out:: Sharp 1
+#define PIN_SHARP_2      A1		// Analog Out:: Sharp 2
+#define PIN_SHARP_3      A2		// Analog Out:: Sharp 3
+#define PIN_SHARP_4      A6		// Analog Out:: Sharp 4	(optional)    	
 
-#define PIN_AM_Y         A6
-#define PIN_AM_S0         6
-#define PIN_AM_S1         7
-#define PIN_AM_S2         8
+#define PIN_AM_S0        12		// Analog Mux: Select 0   	
+#define PIN_AM_S1        A3		// Analog Mux: Select 1   	
+#define PIN_AM_S2        A4		// Analog Mux: Select 2   	
+#define PIN_AM_Y         A7		// Analog Mux: Output
 
-#define PIN_TCRT_LED     13    // IF FEATURE IS ENABLED, THE PIN IS USED FOR TCRT5000 LED TOGGLING
+#define PIN_TCRT_LED     13    	// TCRT LED On/Off switch control
 
-#define PIN_DM_Y         12
-#define PIN_DM_S0         9
-#define PIN_DM_S1        10
-#define PIN_DM_S2        11
+#define PIN_DM_S0         8		// Digital Mux: Select 0
+#define PIN_DM_S1         9		// Digital Mux: Select 1
+#define PIN_DM_S2        10		// Digital Mux: Select 2
+#define PIN_DM_Y         11		// Digital Mux: Output
 
-#define PIN_ECHO_US1      0
-#define PIN_ECHO_US2      1
-#define PIN_ECHO_US3      2
-#define PIN_ECHO_US4      3
-#define PIN_ECHO_US5      4
-#define PIN_ECHO_US6      5
 
-#define PIN_EXT_AN_1     A7
+#define PIN_ECHO_US1      2		// Echo Pin of US1
+#define PIN_ECHO_US2      3		// Echo Pin of US2
+#define PIN_ECHO_US3      4		// Echo Pin of US3
+#define PIN_ECHO_US4      5		// Echo Pin of US4
+#define PIN_ECHO_US5      6		// Echo Pin of US5
+#define PIN_ECHO_US6      7		// Echo Pin of US6
+
+
+/* Extension Header's Available */
+#define PIN_EXT_AN_1     A5
 #define PIN_EXT_AN_2     A3
 #define PIN_EXT_DIG_1    13
 #define PIN_EXT_DIG_2     5
